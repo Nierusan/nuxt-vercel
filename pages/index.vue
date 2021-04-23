29ce10953e284ce9c05373474ff2da111e0737ba
Nielle <Nierusan@users.noreply.github.com>
2021-04-23 20:01:24 +0200

    dev on ipad
    Pushing on vercel

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         nuxtjs
       </h1>
       <h2 class="subtitle">
-        My astonishing Nuxt.js project
+        My astonishing Nuxt.js project on ipad
       </h2>
       <div class="links">
         <a
